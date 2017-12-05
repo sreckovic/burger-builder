2dd5983777af8390dd8db0b1e4ab928b171e0086
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2017-12-05 12:29:32 +0100

    Layout
    Layout

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,8 +4,8 @@ import classes from "./Button.css";
 
 const button = props => (
   <button
-    className={[classes.Button, classes[props.buttonType]].join(" ")}
-    onClick={props.cliked}
+    className={[classes.Button, classes[props.btnType]].join(" ")}
+    onClick={props.clicked}
   >
     {props.children}
   </button>
