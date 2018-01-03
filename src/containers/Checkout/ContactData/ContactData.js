b256d7fb3c87338be2c5053472c011a688159420
Nemanja <nemanja.sreckovic@gmail.com>
2018-01-03 23:17:47 +0100

    Validation
    Validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -110,18 +110,20 @@ class ContactData extends Component {
   };
 
   checkValidity(value, rules) {
-    let isValid = false;
+    let isValid = true;
 
-    if (rules.required) {
-      isValid = value.trim() !== "";
-    }
+    if (rules) {
+      if (rules.required) {
+        isValid = value.trim() !== "" && isValid;
+      }
 
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength;
-    }
+      if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+      }
 
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength;
+      if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+      }
     }
 
     return isValid;
@@ -161,6 +163,8 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
             changed={event => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
