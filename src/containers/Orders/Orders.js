630267e0e34a0528eafccf9ca185508aaef276ba
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-01-10 14:50:51 +0100

    Orders Redux
    Orders Redux

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import Order from "../../components/Order/Order";
 import axios from "../../axios-orders";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+import * as actions from "../../store/actions/index";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Orders extends Component {
-  state = { orders: [], loading: true };
+  /* state = { orders: [], loading: true }; */
 
   componentDidMount() {
+    this.props.onFetchOrders();
+    /*
     axios
       .get("/orders.json")
       .then(res => {
@@ -20,21 +25,41 @@ class Orders extends Component {
       .catch(err => {
         this.setState({ loading: false });
       });
+    */
   }
 
   render() {
-    return (
-      <div>
-        {this.state.orders.map(order => (
-          <Order
-            key={order.id}
-            ingredients={order.ingredients}
-            price={order.price}
-          />
-        ))}
-      </div>
-    );
+    let orders = <Spinner />;
+    if (!this.props.loading) {
+      orders = (
+        <div>
+          {this.props.orders.map(order => (
+            <Order
+              key={order.id}
+              ingredients={order.ingredients}
+              price={order.price}
+            />
+          ))}
+        </div>
+      );
+    }
+    return orders;
   }
 }
 
-export default withErrorHandler(Orders, axios);
+const mapStateToProps = state => {
+  return {
+    orders: state.order.orders,
+    loading: state.order.loading
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onFetchOrders: () => dispatch(actions.fetchOrders())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  withErrorHandler(Orders, axios)
+);
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,2 +1,2 @@
 export { addIgredient, removeIgredient, initIgredients } from "./burgerBuilder";
-export { purchaseBurger, purchaseInit } from "./order";
+export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
