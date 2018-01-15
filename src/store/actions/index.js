d32140746a4de99b54ac1fb779bd179a7bd54112
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-01-15 18:38:27 +0100

    auto login
    auto login

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
 export { addIgredient, removeIgredient, initIgredients } from "./burgerBuilder";
 export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
-export { auth, logout, setAuthRedirectPath } from "./auth";
+export { auth, logout, setAuthRedirectPath, authCheckState } from "./auth";
