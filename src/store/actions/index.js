ab9c51556aceff17e6e464c36a806b1bd390261d
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2018-01-19 15:45:44 +0100

    checkAuthTimeoutSaga
    checkAuthTimeoutSaga

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,9 @@
 export { addIgredient, removeIgredient, initIgredients } from "./burgerBuilder";
 export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
-export { auth, logout, setAuthRedirectPath, authCheckState } from "./auth";
+export {
+  auth,
+  logout,
+  setAuthRedirectPath,
+  authCheckState,
+  logoutSucceed
+} from "./auth";
